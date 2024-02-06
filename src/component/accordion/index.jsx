7f22ae69b14815ff3c32167832ec3332968c4da0
Shivrag <shivragshukla001@gmail.com>
2024-02-06 12:50:03 +0530

    Added: multiple select accordion

--- src/component/accordion/index.jsx
+++ src/component/accordion/index.jsx
@@ -5,28 +5,62 @@ import './styles.css'
 export default function Accordion() {
 
 	const [selectedId, setSelectedId] = useState(null);
+	const [enableMultiple, setEnableMultiple] = useState(false);
+	const [multipleId, setMultipleId] = useState([]);
 
 	function handleSingleSelect(id) {
 		setSelectedId(id);
 	}
+
+	function handleMultipleSelect(id) {
+		var localMultipleId = [...multipleId];
+		var index = localMultipleId.indexOf(id);
+		localMultipleId.indexOf(id) !== -1
+		? localMultipleId.splice(index, 1)
+		: localMultipleId.push(id)
+
+		setMultipleId(localMultipleId);
+	}
+
+	function handleSelect(id) {
+		enableMultiple ? handleMultipleSelect(id) : handleSingleSelect(id);
+	}
+
+	console.log(multipleId)
 	
 	return (
 		<div className="container">
 			<div className="accordion">	
 				<h1>Accordion</h1>
+				<button onClick={() => setEnableMultiple(!enableMultiple)}> 
+					{
+						enableMultiple 
+						? <span> Enable single</span>
+						: <span> Enable multiple</span>
+					}
+				</button>
 				{
 					data && data.length > 0 
 					?
 					data.map(item =>
 						<div className="item" key={item.id}>
-							<div onClick={() => handleSingleSelect(item.id)} className="title">
+							<div onClick={() => handleSelect(item.id)} className="title">
 								<h3>{item.title}</h3>								
 								<span>+</span>
 							</div>
 							{
-								selectedId === item.id 
-								? <div className="body">{item.body}</div>
-								: ''
+								enableMultiple 
+								?  (
+										multipleId.indexOf(item.id) !== -1
+										? <div className="body">{item.body}</div>
+										: ''
+									)
+								: 	(
+										selectedId === item.id 
+										? <div className="body">{item.body}</div>
+										: ''
+
+									)
 							}
 							
 						</div>
